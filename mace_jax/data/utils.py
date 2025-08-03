e18ec16628fb2af1ee59a9ac02ace70b275c7914
abhijeetgangan <abhijeet.gangan261997@gmail.com>
2025-08-03 00:04:55 -0700

    Add compute_average_E0s

--- mace_jax/data/utils.py
+++ mace_jax/data/utils.py
@@ -1,6 +1,9 @@
-from typing import Tuple
+import logging
+from typing import Dict, Tuple
 
 import jax.numpy as jnp
+import numpy as np
+from ase import Atoms
 
 
 def get_edge_vectors_and_lengths(
@@ -39,3 +42,33 @@ def get_edge_vectors_and_lengths(
     if normalize:
         edge_vectors = edge_vectors / (edge_lengths[..., None] + eps)
     return edge_vectors, edge_lengths
+
+
+def compute_average_E0s(
+    collections_train: list[Atoms], z_table: list[int]
+) -> Dict[int, float]:
+    """
+    Function to compute the average interaction energy of each chemical element
+    returns dictionary of E0s
+    """
+    len_train = len(collections_train)
+    len_zs = len(z_table)
+    A = np.zeros((len_train, len_zs))
+    B = np.zeros(len_train)
+    for i in range(len_train):
+        B[i] = collections_train[i].get_potential_energy()
+        for j, z in enumerate(z_table):
+            A[i, j] = np.count_nonzero(collections_train[i].get_atomic_numbers() == z)
+    try:
+        E0s = np.linalg.lstsq(A, B, rcond=None)[0]
+        atomic_energies_dict = {}
+        for i, z in enumerate(z_table):
+            atomic_energies_dict[z] = E0s[i]
+    except np.linalg.LinAlgError:
+        logging.error(
+            "Failed to compute E0s using least squares regression, using the same for all atoms"
+        )
+        atomic_energies_dict = {}
+        for i, z in enumerate(z_table):
+            atomic_energies_dict[z] = 0.0
+    return atomic_energies_dict
